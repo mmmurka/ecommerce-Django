7d9956903e4d27c1e0b1f225bd1456f3ab692ab4
mmmurka <murenkodan2005@gmail.com>
2023-10-16 12:00:58 +0300

    add category and product page

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop.context_processors.menu_links',
             ],
         },
     },
@@ -124,6 +125,11 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'static' / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = category_page.product_set.filter(available=True)
+    else:
+        products = Product.objects.filter(available=True)
+    return render(request, 'home.html', {'category': category_page, 'products': products})
 
-def product(request):
-    return render(request, 'product.html')
+def product(request, category_slug, product_slug):
+    product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+    return render(request, 'product.html', {'product': product})
+
+
+def cart(request):
+    return render(request, 'cart.html')
